f926ea32f3d026fbdb3443da6f2a45802c665b9d
Anthony DeMatteo <dematta@sunypoly.edu>
2024-09-25 19:59:51 -0400

    Major fix: normalize_histogram function was returning a scalar, not an array (despite norm_hist and cdf function passing tests), resulting in test failures for transformation function.

--- A01.py
+++ A01.py
@@ -17,11 +17,15 @@ def create_unnormalized_hist(image):
 def normalize_hist(hist):
     # total number of pixels in histogram
     histSum = np.sum(hist)
-    # iterate through each pixel intensity, dividing each by histSum to normalize
-    for i in range(len(hist)):
-       nhist = hist[i] / histSum
+    # Create a new array to hold the normalized histogram
+    nhist = np.zeros(256, dtype=np.float64)
     
-    return nhist
+    # Iterate through each pixel intensity, dividing each by histSum to normalize
+    for i in range(len(hist)):
+        nhist[i] = hist[i] / histSum  
+        
+    return nhist 
+
 
 def create_cdf(nhist):
     # cdf is  a numpy array of shape (256,) and type "float64"
@@ -45,11 +49,15 @@ def get_hist_equalize_transform(image, do_stretching):
     int_transform = np.zeros(256, dtype=np.float64)
     
     # stretching formula is newImage = ((oldPixelIntensity - minIntensity) * 255) / maxIntensity - minIntensity
-    if do_stretching == True:
-        for i in range(len(cdf)):
-            int_transform[i] = ((cdf[i] - cdf.min()) * 255) / (cdf.max() - cdf.min())
+    if do_stretching:
+        cdfMin = cdf.min()
+        cdfMax = cdf.max()
+        cdf = (cdf - cdfMin) * 255 / (cdfMax - cdfMin)
+    else:
+        cdf *= 255
+        #cdf = cdf * 255
         
-    int_transform = cv2.convertScaleAbs(int_transform)[:,0]
+    int_transform = cv2.convertScaleAbs(cdf)[:,0]
     
     return int_transform
         
