39e6f557d2ca509e3bafee827ad9be0e98a4264f
Anthony DeMatteo <dematta@sunypoly.edu>
2024-09-25 12:18:44 -0400

    assignment01 progress

---
+++ A01.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+def create_unnormalized_hist(image):
+    # Create a numpy array of zeros with shape (256,) and dtype float64
+    hist = np.zeros(256, dtype=np.float64)
+    # Iterate through each pixel in the image (rows and columns)
+    height, width = image.shape
+    for i in range(height):
+        for j in range(width):
+            pixel = image[i, j]  
+            hist[pixel] += 1  # to get intensity values
+    
+    return hist
+
+
+def normalize_hist(hist):
+    # total number of pixels in histogram
+    histSum = np.sum(hist)
+    # iterate through each pixel intensity, dividing each by histSum to normalize
+    for i in range(len(hist)):
+       nhist = hist[i] / histSum
+    
+    return nhist
+
+def create_cdf(nhist):
+    # cdf is  a numpy array of shape (256,) and type "float64"
+    cdf = np.zeros(256, dtype=np.float64)
+    # cSum is cumulative sum
+    cSum = 0
+    for i in range(len(nhist)):
+        cSum += nhist[i] 
+        cdf[i] = cSum
+        
+    return cdf
+
+def get_hist_equalize_transform(image, do_stretching):
+    # Use create_unnormalized_hist to calculate the unnormalized histogram 
+    hist = create_unnormalized_hist(image)
+    # Normalize your histogram
+    nhist = normalize_hist(hist)
+    # Make the CDF
+    cdf = create_cdf(nhist)
+    
+    if do_stretching == True:
+        return None
+        
+    
+    
+
+
+    
+
+
+    
