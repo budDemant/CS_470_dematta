b6bb315eb33ca5bc219364df40109f78bba494b2
Dr. Michael J. Reale <realemj@sunypoly.edu>
2024-10-02 13:53:02 -0400

    Hough lines example

--- ProfExercises05.py
+++ ProfExercises05.py
@@ -26,6 +26,55 @@ import torch
 import cv2
 import pandas
 import sklearn
+import math as m
+
+def draw_polar_line(image, line):
+    # (p, theta)
+    p, theta = line
+    
+    # x cos theta + y sin theta = p
+    # Ax + By = p
+    
+    A = m.cos(theta)
+    B = m.sin(theta)
+    
+    if abs(B) >= 1e-6:
+        # Ax + By = p
+        # Start: x = 0
+        #    By = p
+        #    y = p/B
+        
+        # End: x = width
+        #    A*width + By = p
+        #    By = p - A*width
+        #    y = (p - A*width)/B
+        
+        start_y = p / B
+        end_y = (p - A*image.shape[1])/B
+        
+        start_y = int(start_y)
+        end_y = int(end_y)
+        
+        cv2.line(image, (0, start_y), (image.shape[1], end_y), (0,0,255), 3)
+    else:
+        # Ax + By = p
+        # Start: y = 0
+        #     Ax = p
+        #     x = p/A
+        
+        # End: y = height
+        #     Ax + B*height = p
+        #     Ax = p - B*height
+        #     x = (p - B*height)/A
+        
+        start_x = p/A
+        end_x = (p - B*image.shape[0])/A
+        
+        start_x = int(start_x)
+        end_x = int(end_x)
+        
+        cv2.line(image, (start_x, 0), (end_x, image.shape[0]), (0,0,255), 3)
+        
 
 ###############################################################################
 # MAIN
@@ -81,12 +130,20 @@ def main():
         high_T = 200
         low_T = 100
         
+        hthresh = 100
+        
         while key != ESC_KEY:
             # Get next frame from camera
             _, image = camera.read()
             
             grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             canny = cv2.Canny(grayscale, low_T, high_T)
+            
+            lines = cv2.HoughLines(canny, rho=1, theta=m.pi/180.0, threshold=hthresh)
+            #print(lines)
+            if lines is not None:
+                for line in lines:
+                    draw_polar_line(image, line[0])
             
             # Show the image
             cv2.imshow(windowName, image)
@@ -95,11 +152,15 @@ def main():
             # Wait 30 milliseconds, and grab any key presses
             key = cv2.waitKey(30)
             
+            if key == ord('e'): hthresh += 1
+            if key == ord('d'): hthresh -= 1
+                        
             if key == ord('q'): high_T += 1
             if key == ord('a'): high_T -= 1
             if key == ord('w'): low_T += 1
             if key == ord('s'): low_T -= 1
-            print(low_T, high_T)
+            if lines is not None: print("Line cnt:", len(lines))
+            print(low_T, high_T, hthresh)
 
         # Release the camera and destroy the window
         camera.release()
