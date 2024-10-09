5162432b10a75163496471e301885ca4edd1ec62
Anthony DeMatteo <dematta@sunypoly.edu>
2024-10-09 13:03:09 -0400

    Gradio

--- A02.py
+++ A02.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import gradio as gr
 
 def read_kernel_file(filepath):
     # Open a file for reading and grab the first line
@@ -67,3 +68,25 @@ def apply_filter(image, kernel, alpha=1.0, beta=0.0, convert_uint8=True):
 
         
     return output
+
+def filtering_callback(input_img, filter_file, alpha_val, beta_val): 
+    input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY) 
+    kernel = read_kernel_file(filter_file.name) 
+    output_img = apply_filter(input_img, kernel, alpha_val, beta_val)     
+    return output_img 
+ 
+    
+def main(): 
+    demo = gr.Interface(fn=filtering_callback,  
+                        inputs=["image",  
+                                "file",  
+                                gr.Number(value=0.125),  
+                                gr.Number(value=127)], 
+                        outputs=["image"]) 
+    demo.launch()   
+    
+# Later, at the bottom 
+if __name__ == "__main__":  
+    main()   
+ 
+    
