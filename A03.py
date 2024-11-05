a1baeb7efcfb8c16878e79b7cb2c38c506cdacd4
Anthony DeMatteo <dematta@sunypoly.edu>
2024-11-05 00:41:08 -0500

    Fixed syntax errors for: - import skimage - skimage.segmentation.slice (and parameters)

--- A03.py
+++ A03.py
@@ -2,13 +2,13 @@
 
 
 import cv2
-from skimage import slic
+import skimage
 import numpy as np
 
 def find_WBC(image):
     
     #(Step 1) Get superpixel groups. Assume the image with superpixel indices is given by segments.
-    segments = slic(image, nSegments=100, startLabel=0)
+    segments = skimage.segmentation.slic(image, n_segments=100, start_label=0)
     # number of groups found can then be extracted using:
     cnt = len(np.unique(segments))
     
