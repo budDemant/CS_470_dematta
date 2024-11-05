4b53681c3db03c4ce4e243b72cade11cc152eea0
Anthony DeMatteo <dematta@sunypoly.edu>
2024-11-04 22:11:11 -0500

    Following example. Completed Steps 1 and 2.

--- A03.py
+++ A03.py
@@ -1,4 +1,23 @@
 
-# Watch ProfExercise part of Lecture 10.28.2024
 
-# color segmentation or something for borders, and then "cleaning up" with opening and close morphological
+
+import cv2
+from skimage import slic
+import numpy as np
+
+def find_WBC(image):
+    #(Step 1) Get superpixel groups. Assume the image with superpixel indices is given by segments.
+    segments = slic(image, nSegments=100, startLabel=0)
+    # number of groups found can then be extracted using:
+    cnt = len(np.unique(segments))
+    
+    #(Step 2) Compute mean color per superpixel.
+    groupMeans = np.zeros((cnt, 3), dtype="float32")
+    for specificGroup in range(cnt):
+        maskImage = np.where(segments == specificGroup, 255, 0).astype("uint8") 
+        maskImage = np.expand_dims(maskImage)
+        groupMeans[specificGroup] = cv2.mean(image, mask=maskImage)[0:3]
+        
+    
+    
+    
