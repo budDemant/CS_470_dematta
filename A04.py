bd1f3baa8533744304921ee3a5d99c3dc0266432
Anthony DeMatteo <dematta@sunypoly.edu>
2024-11-18 15:50:48 -0500

    Started getOneLBPLabel function.

--- A04.py
+++ A04.py
@@ -1,2 +1,14 @@
+import numpy as np
 
-#test
+def getOneLBPLabel(subimage):
+    # Ensure the input is a 3x3 subimage (maybe unnecessary given that all test inputs match this)
+    # assert subimage.shape == (3, 3), "Input must be a 3x3 subimage."
+    
+    # Get center pixel
+    centerPixel = subimage[1, 1]
+    
+    # binary pattern by comparing neighbors to the center pixel
+    binaryPattern = (subimage > centerPixel).astype(int)
+    
+    
+    #return lbpLabel
