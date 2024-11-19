124522defbd7829283798857bf9357db18419819
Anthony DeMatteo <dematta@sunypoly.edu>
2024-11-18 19:46:16 -0500

    getLBPImage: loop

--- A04.py
+++ A04.py
@@ -59,4 +59,14 @@ def getLBPImage(image):
     height, width = image.shape
     lbpImage = np.zeros((height,width), dtype=np.uint8)
     
-    
+    # Loop through each pixel in the image, cut out the appropriate subimage...
+    for i in range(1, height + 1):
+        for j in range(1, width + 1):
+            # 3x3 neighborhood extracted for each pixel
+            subimage = paddedImage[i-1:i+2, j-1:j+2]
+            
+            lbpLabel = getOneLBPLabel(subimage)
+            
+            lbpImage[i-1, j-1] = lbpLabel
+            
+    return lbpImage
